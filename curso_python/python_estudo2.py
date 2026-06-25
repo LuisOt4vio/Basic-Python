7b8612ba28c587647e04454c300783648b6baca9
Luís Otávio <luis.otavioc13@gmail.com>
2026-06-25 00:11:45 -0300

    Basic Comands Python v4

---
+++ curso_python/python_estudo2.py
@@ -0,0 +1,25 @@
+condicao = True
+
+if condicao:
+    print ('Essa condição está dentro do IF')
+
+print ('Está fora do IF')
+
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print ('Essa condição está dentro do IF')
+elif condicao2:
+    pass #Comando utilizado para passar, serve como se fosse um "DEPOIS EU ESCROVO O CÓDIGO"
+elif condicao3:
+    pass
+elif condicao4:
+    pass
+else:
+    print('Condição não foi seguida.')
+
+
---
+++ curso_python/python_estudo3.py
@@ -0,0 +1,14 @@
+maior = 2 > 1
+maior_igual = 2 >= 2
+menor = 1 < 2
+menor_igual = 2 <= 2
+igual = 'b' == 'b'
+diferente = 'a' != 'b'
+
+print(maior)
+print(maior_igual)
+print(menor)
+print(menor_igual)
+print(igual)
+print(diferente)
+
