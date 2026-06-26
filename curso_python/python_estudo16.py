c853b14461cc486a2f3bc9443554088e73f0a474
Luís Otávio <luis.otavioc13@gmail.com>
2026-06-26 16:11:24 -0300

    Basic Comands Python v1

--- curso_python/python_estudo16.py
+++ curso_python/python_estudo16.py
@@ -1,8 +1,10 @@
 frase = input('Digite uma frase: ')
-i = 0
+i = 0 ; j = 0
 cont_letra_vezes = 0
 letra_apareceu_mais = ''
 letra_apareceu_mais_vezes = ''
+nova_string = ''
+novo_nome = ''
 while i < len(frase):
     atual_letra = frase[i]
     cont_letra = frase.count(f'{atual_letra}')
@@ -11,10 +13,28 @@ while i < len(frase):
         i+=1
         continue
 
+    if cont_letra == cont_letra_vezes and atual_letra not in letra_apareceu_mais:
+            letra_apareceu_mais += atual_letra
+
     if cont_letra > cont_letra_vezes:
         cont_letra_vezes = cont_letra
         letra_apareceu_mais = atual_letra
 
     i += 1
- 
-print(f'A letra que apareceu mais vezes foi ({letra_apareceu_mais}) tendo um total de {cont_letra_vezes}x')
+
+qntd = len(letra_apareceu_mais)
+
+while j < qntd:
+    nova_string = f'{letra_apareceu_mais[j]}' 
+    novo_nome += nova_string
+    if j < qntd-1:
+        novo_nome += ','      
+    j+=1            
+
+
+
+if qntd >= 2:     
+    print(f'As letras que mais apareceram foram ({novo_nome}) tendo um total de {cont_letra_vezes}x cada uma delas.')
+else:
+    print(f'A letra que apareceu mais vezes foi ({letra_apareceu_mais}) tendo um total de {cont_letra_vezes}x.')
+     
